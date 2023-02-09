fc81b81d96ac31c0e27992621e6c31447673d960
Daniel Grayson <graysoncodes@gmail.com>
2023-02-09 12:33:44 +0100

    updated web view

--- portfolio_website/lib/screens/web_view.dart
+++ portfolio_website/lib/screens/web_view.dart
@@ -11,7 +11,62 @@ class _WebLayoutState extends State<WebLayout> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: const Text(
+          'DANIEL GRAYSON',
+          style: TextStyle(fontSize: 15, color: Colors.white),
+        ),
+        centerTitle: true,
+        leading: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: const [
+            Text(
+              'PROJECTS',
+              style: TextStyle(fontSize: 15, color: Colors.white),
+            ),
+            Text(
+              'CONTACT',
+              style: TextStyle(fontSize: 15, color: Colors.white),
+            ),
+            Text(
+              'RESUME',
+              style: TextStyle(fontSize: 15, color: Colors.white),
+            )
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.camera,
+              color: Colors.white,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.camera,
+              color: Colors.white,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.camera,
+              color: Colors.white,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.camera,
+              color: Colors.white,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
